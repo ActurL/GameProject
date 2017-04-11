2c7a184db35235e2494778636540565d561387be
ActurL <mihailo.dilparic@gmail.com>
2017-04-11 20:45:44 +0200

    Add files via upload

---
+++ NPC Generator.py
@@ -0,0 +1,28 @@
+#NPC GENERATOR
+
+from Name_Gen import names
+from Stat_Gen import stat_gen
+import os.path
+import random
+
+if os.path.isfile("Names.txt") and os.path.isfile("Surnames.txt"): #Check if name files exist;
+    pass
+else:
+    names()
+
+class NPC :
+
+    def name_data(self):
+
+        self.name = random.choice(open("Names List.txt", 'r').readlines())
+        name = self.name
+
+        return name
+
+    def stats_data(self):
+        self.stats = stat_gen()
+        stats = self.stats
+
+        return stats
+
+
