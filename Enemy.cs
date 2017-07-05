50b8da0129f9aadb79326e259a32eabb70d14786
TomAppru <tca6379@gmail.com>
2017-07-05 08:18:46 +0100

    Add files via upload

---
+++ Enemy.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace battleTest
+{
+    class Enemy : Entity
+    {
+        private string[][] monsterDetails;
+        private string[] monster;
+        private Random rand = new Random();
+        private string type;
+
+        public Enemy() : base()
+        {
+            //Takes enemies from a text file. Not sure if I'll keep that format but it works.
+            monsterDetails = fileRead("Enemies");
+            monster = monsterDetails[rand.Next(monsterDetails.Length)];
+
+            //Assigns values to the enemy's properties
+            type = monster[0];
+            name = monster[1];
+            maxHP = int.Parse(monster[2]);
+            minDamage = int.Parse(monster[3]);
+            maxDamage = int.Parse(monster[4]);
+            HP = maxHP;
+
+        }
+
+        private string[][] fileRead(string fileName)
+        {
+            //Reads enemy text file
+            string[] line;
+            string[] fileLines;
+            char[] delimeters = { ',' };
+
+            fileLines = System.IO.File.ReadLines(fileName + ".txt").ToArray();
+            string[][] separatedFileLines = new string[fileLines.Length][];
+
+            for (int i = 0; i < fileLines.Length; i++)
+            {
+                line = fileLines[i].Split(delimeters, StringSplitOptions.RemoveEmptyEntries);
+                line[line.Length - 1] = line[line.Length - 1].Replace("\n", "");
+                separatedFileLines[i] = line;
+            }
+
+            return separatedFileLines;
+        }
+
+        public string getType()
+        {
+            return type;
+        }
+    }
+}
---
+++ battler.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace battleTest
+{
+    public partial class battleWindow : Form
+    {
+        public battleWindow()
+        {
+            InitializeComponent();
+
+            //Placeholder, just inventory for show. Need item class, subclasses for weapons etc.
+            dgvInventory.Rows.Add("Rusty Sword", "x1");
+            dgvInventory.Rows.Add("Small Healing Potion", "x5");
+            dgvInventory.Rows.Add("Beaten Shield", "x1");
+
+            //Again, placeholder. Creates random enemy.
+            Enemy enemey = new Enemy();
+
+            //Displays health. I'll move that to a proc so I can run it after attacks too.
+            setHealthLabel(enemey.getHP(), enemey.getMaxHP(), lblMonsterHP);
+            setHealthLabel(20, 20, lblPlayerHP);
+
+            //formatting combobox
+            populateComboBox();
+            cmbItemToUse.SelectedIndex = 0;
+            rtbOutput.AppendText("A wild " + enemey.getName() + " appeared!");
+        }
+
+        private void populateComboBox()
+        {
+            //proc to move inventory items into combobox
+            for (int i = 0; i < dgvInventory.RowCount; i++)
+            {
+                cmbItemToUse.Items.Add(dgvInventory.Rows[i].Cells[0].Value);
+            }
+        }
+
+        private void setHealthLabel(int HP, int maxHP, Label label)
+        {
+            //displays HP
+            label.Text = HP + "/" + maxHP;
+        }
+    }
+}
